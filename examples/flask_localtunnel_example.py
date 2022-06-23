6c05d49f8428f973f19a456781a3113e9bbca400
jakbin <jakbin4747@gmail.com>
2022-06-23 12:26:51 +0530

    update example

--- examples/flask_localtunnel_example.py
+++ examples/flask_localtunnel_example.py
@@ -4,11 +4,9 @@ from flask_lt import run_with_lt
 app = Flask(__name__)
 run_with_lt(app)
 
-
 @app.route("/")
 def hello():
     return "Hello World!"
-
 
 if __name__ == '__main__':
     app.run()
